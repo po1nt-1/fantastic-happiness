029ea3c56d1543ff8a990653f03d8b3c5119687f
po1nt <57108227+po1nt-1@users.noreply.github.com>
2022-12-12 01:57:47 +1000

    Dev (#2)
    getting a URL to images sent by the user
    
    - added gosec linter action
    - added yaml linter action
    - added CI build action

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # fantastic-happiness
+https://github.com/po1nt-1/fantastic-happiness/actions/workflows/ci.yml/badge.svg
--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"fmt"
-
 	"fantastic-happiness/internal/bot"
 )
 
 func main() {
-	fmt.Println("hello from main.go")
 	bot.Run()
 }
