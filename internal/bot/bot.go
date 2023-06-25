e0fba5b78e7fc38deec7f5b221aeac8d7ede7edb
po1nt <po1nt.sploit@gmail.com>
2023-06-26 02:11:42 +1000

    fixed image processing, fixed logging

--- internal/bot/bot.go
+++ internal/bot/bot.go
@@ -1,6 +1,7 @@
 package bot
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/po1nt-1/fantastic-happiness/internal/config"
@@ -11,7 +12,7 @@ import (
 func Run() {
 	bot, err := tgBotApi.NewBotAPI(config.Config.Tg.Token)
 	if err != nil {
-		log.Fatalf("NewBotAPI: %v", err)
+		log.Fatalf("tgBotApi.NewBotAPI: %v", err)
 	}
 	bot.Debug = config.Config.Tg.Debug
 	log.Printf("Authorized on account: %v", bot.Self.UserName)
@@ -32,24 +33,22 @@ func Run() {
 		fileID := msg.Photo[len(msg.Photo)-1].FileID
 		fileURL, err := bot.GetFileDirectURL(fileID)
 		if err != nil {
-			log.Printf("GetFileDirectURL: %v", err)
+			log.Printf("bot.GetFileDirectURL: %v", err)
+			continue
 		}
 
 		responseText, err := processImage(fileURL)
 		if err != nil {
-			log.Printf("processImage: %v", err)
-		}
-		if responseText == "" {
-			continue
+			log.Printf("processImage: %v: %v", err, responseText)
+			responseText = fmt.Sprintf("server error: %v", responseText)
 		}
 
 		responseText = tgBotApi.EscapeText(tgBotApi.ModeMarkdownV2, responseText)
 		response := tgBotApi.NewMessage(msg.Chat.ID, responseText)
 		response.ReplyToMessageID = msg.MessageID
 		response.ParseMode = tgBotApi.ModeMarkdownV2
-
 		if _, err := bot.Send(response); err != nil {
-			log.Printf("Send: %v", err)
+			log.Printf("bot.Send: %v", err)
 		}
 	}
 }
--- internal/bot/qr.go
+++ internal/bot/qr.go
@@ -13,19 +13,19 @@ import (
 func downloadImage(fileURL string) (bytes.Buffer, error) {
 	res, err := http.Get(fileURL) //nolint:gosec
 	if err != nil {
-		log.Println(err)
+		log.Fatalf("http.Get: %v", err)
 	}
 	defer func() {
 		err := res.Body.Close()
 		if err != nil {
-			log.Println(err)
+			log.Fatalf("res.Body.Close: %v", err)
 		}
 	}()
 
 	imageBuffer := bytes.Buffer{}
 	_, err = io.Copy(&imageBuffer, res.Body)
 	if err != nil {
-		log.Println(err)
+		log.Fatalf("io.Copy: %v", err)
 	}
 
 	return imageBuffer, nil
@@ -34,7 +34,8 @@ func downloadImage(fileURL string) (bytes.Buffer, error) {
 func zbarRun(input bytes.Buffer) (string, error) {
 	zbarimgPath, err := exec.LookPath("zbarimg")
 	if err != nil {
-		log.Println(err)
+		log.Printf("exec.LookPath: %v", err)
+		return "", err
 	}
 
 	args := []string{
@@ -49,32 +50,27 @@ func zbarRun(input bytes.Buffer) (string, error) {
 
 	output, err := zbarimgCmd.CombinedOutput()
 	if err != nil {
-		log.Println(err)
+		return "barcode data was not detected", err
 	}
 
 	outputString := fmt.Sprint(string(output), err)
 	outputString = strings.Replace(outputString, "QR-Code:", "", 1)
-	outputSlice := strings.Split(outputString, "\n")
+	outputString = strings.Split(outputString, "\n")[0]
+	outputString = string(outputString)
 
-	result := ""
-	for _, line := range outputSlice {
-		if strings.Contains(line, "\n") {
-			result += line + "\n"
-		}
-	}
-
-	return result, nil
+	return string(outputString), nil
 }
 
 func processImage(fileURL string) (string, error) {
 	imageBuffer, err := downloadImage(fileURL)
 	if err != nil {
+		log.Printf("downloadImage: %v", err)
 		return "", err
 	}
 
 	result, err := zbarRun(imageBuffer)
 	if err != nil {
-		log.Println(err)
+		return result, err
 	}
 
 	return result, nil
